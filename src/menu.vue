a1bc877b29b62239018375927185ee89cd761c02
Hsiaoming Yang <me@lepture.com>
2015-10-27 14:39:37 +0800

    Init menu component

---
+++ src/menu.vue
@@ -0,0 +1,25 @@
+<template>
+<button class="v-menu" :class="{'v-menu--open':open}" v-on:click.prevent="open=!open">
+  <span><slot>Menu</slot></span>
+</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {open: false}
+  }
+}
+</script>
+
+<style>
+button.v-menu {
+  border: none;
+}
+button.v-menu:before,
+button.v-menu:after {
+}
+button.v-menu > span {
+  overflow: hidden;
+}
+</style>
