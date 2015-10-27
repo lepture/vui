3a7f9a7d03512fa5ebf9b20d5296f0c6f9214f2d
Hsiaoming Yang <me@lepture.com>
2015-10-27 14:44:24 +0800

    Prepare dependencies and webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require("webpack");
+var vue = require("vue-loader");
+var cssLoader = "style-loader!css-loader?sourceMap!postcss-loader";
+
+var publicPath = "/build/";
+
+module.exports = {
+  entry: {
+    app: ["index.js"],
+    vendor: ["vue", "vue-router"],
+  },
+
+  output: {
+    path: __dirname + publicPath,
+    filename: 'app.js',
+    publicPath: publicPath,
+  },
+
+  module: {
+    loaders: [
+      {test: /\.vue$/, loader: vue.withLoaders({css: cssLoader})},
+      {test: /\.css$/, loader: cssLoader},
+    ]
+  },
+
+  plugins: [
+      new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js"),
+  ],
+
+  postcss: function () {
+    // use webpack context
+    var postcssImport = require('postcss-import');
+
+    return [
+      postcssImport({
+        onImport: function (files) {
+          files.forEach(this.addDependency);
+        }.bind(this)
+      }),
+
+      require('postcss-custom-properties'),
+    ]
+  },
+
+  devtool: "source-map",
+};
