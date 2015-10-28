094aaffec8f369ecd4f087ac0900081148ac27c4
Hsiaoming Yang <me@lepture.com>
2015-10-28 14:41:19 +0800

    Add slider range

---
+++ src/slider-range.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="v-slider-range">
+  <slot name="left"></slot>
+  <input type="range" v-model="value" :min="min" :max="max" :step="step" :name="name">
+  <slot name="right"></slot>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    min: {
+      type: Number,
+    },
+    max: {
+      type: Number,
+    },
+    step: {
+      type: Number,
+    },
+    name: {
+      type: String,
+    },
+    value: {
+      type: Number,
+    }
+  }
+}
+</script>
+
+<style>
+.v-slider-range {
+  display: flex;
+  align-items: center;
+  padding: 2px 11px;
+}
+.v-slider-range input[type=range] {
+  -webkit-appearance: none;
+  flex: 1;
+  display: block;
+  overflow: hidden;
+  margin: 5px 10px;
+  padding: 16px 1px 20px;
+  box-sizing: border-box;
+  outline: none;
+  -webkit-tap-highlight-color: transparent;
+}
+.v-slider-range input[type=range]::-webkit-slider-runnable-track {
+  position: relative;
+  height: 2px;
+  background: #ddd;
+  border: none;
+  border-radius: 3px;
+}
+.v-slider-range input[type=range]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  position: relative;
+  margin-top: -12px;
+  cursor: pointer;
+  width: 28px;
+  height: 28px;
+  border: 0;
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0 0 1px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,0.2);
+}
+.v-slider-range input[type=range]::-webkit-slider-thumb:before {
+  position: absolute;
+  display: inline-block;
+  content: '';
+  top: 12px;
+  left: -2001px;
+  width: 2000px;
+  height: 2px;
+  background: rgba(0, 0, 0, 0.6);
+}
+</style>
