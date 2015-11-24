748e478a9fab83d780aa66d3a02ac8d3ad07e3ff
Hsiaoming Yang <me@lepture.com>
2015-11-25 01:15:22 +0800

    Remove touchmove on document. Add close button

--- view/Overlay.vue
+++ view/Overlay.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="overlay">
     <div class="overlay__mask" @click="show=false"></div>
+    <button class="overlay__close" aria-label="关闭" @click="show=false">
+      <svg viewBox="0 0 40 40" tabindex="-1">
+        <circle cx="20" cy="20" r="18"  stroke="#ccc" stroke-width="1" fill-opacity="0"/>
+        <line x1="15" y1="15" x2="25" y2="25" stroke="#dadada" stroke-width="2"/>
+        <line x1="15" y1="25" x2="25" y2="15" stroke="#dadada" stroke-width="2"/>
+      </svg>
+    </button>
     <div class="overlay__content">
       <slot name="overlay-hide"></slot>
       <slot></slot>
@@ -10,17 +17,19 @@
 
 <script>
 export default {
-  props: ['show'],
+  props: {
+  show: {
+      twoWay: true
+    }
+  },
   methods: {
     open() {
       var el = document.querySelector('html')
       el.classList.add('overlay--open')
-      document.addEventListener('touchmove', disableScroll, false)
     },
     close() {
       var el = document.querySelector('html')
       el.classList.remove('overlay--open')
-      document.removeEventListener('touchmove', disableScroll, false)
     },
   },
   ready() {
@@ -41,10 +50,6 @@ export default {
     }
   }
 }
-
-function disableScroll(e) {
-  e.preventDefault()
-}
 </script>
 
 <style>
@@ -54,8 +59,8 @@ function disableScroll(e) {
   justify-content: center;
   left: 0;
   top: 0;
-  width: 100%;
-  height: 100%;
+  right: 0;
+  bottom: 0;
   background-color: rgba(255, 255, 255, 0.95);
   z-index: 9999;
 }
@@ -78,11 +83,33 @@ function disableScroll(e) {
 .overlay__content {
   position: relative;
   max-width: 860px;
+  height: 100%;
   z-index: 22;
   flex: 1;
 }
-/* slot=overlay-hide is an extra part to hide overlay */
-.overlay [slot="overlay-hide"] {
+button.overlay__close {
   position: absolute;
+  display: block;
+  border: none;
+  background: none;
+  top: 20px;
+  right: 30px;
+  width: 60px;
+  height: 60px;
+  z-index: 99;
+  cursor: pointer;
+  outline: none;
+  animation: fade-in 1s cubic-bezier(1, 0.06, 0.74, 1);
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+button.overlay__close svg {
+  width: 40px;
+  height: 40px;
+  outline: none;
+}
+@media (max-width: 680px) {
+  .overlay button.overlay__close {
+    right: 20px;
+  }
 }
 </style>
